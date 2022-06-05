55d4affaf1b0f3596af962ec99fb10f0cd0a568f
iqbalwlyddn <iqblaaa@gmail.com>
2022-06-05 15:13:47 +0700

    initial commit

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, fnSort) => {
+  sortData = fnSort(arr)
+
+  let data = []
+  for (let x = 0; x < sortData.length; x++) {
+    let loop = ''
+    loop += x + 1 + '. ' + sortData[x]
+    data.push(loop)
+  }
+  return data
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  let data = arr.sort()
+  return data
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  let data = arr.sort().reverse();
+  return data
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
